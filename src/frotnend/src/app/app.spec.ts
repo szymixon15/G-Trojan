9665bb99c29cab964e52d659f6581e62d4ce8cd0
xxxDKGxxx <dominikzielinski678@gmail.com>
2025-10-04 11:13:38 +0200

    fix

--- src/frotnend/src/app/app.spec.ts
+++ src/frotnend/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, hackyeah_project');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frotnend');
   });
 });
